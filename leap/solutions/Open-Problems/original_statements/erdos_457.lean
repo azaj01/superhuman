f9c7797bcc533c342dcdc16db1419ed5765cffed
Dawsen Hwang <dawsen@google.com>
2026-06-04 16:42:10 -0500

    Revert changes to correct author attribution
    PiperOrigin-RevId: 926896270

--- leap/solutions/Open-Problems/original_statements/erdos_457.lean
+++
@@ -1,6 +0,0 @@
-import Mathlib
-
-theorem erdos_457 : ∃ ε > (0 : ℝ),
-    { (n : ℕ) | ∀ (p : ℕ), p ≤ (2 + ε) * Real.log n → p.Prime →
-      p ∣ ∏ i ∈ Finset.Icc 1 ⌊Real.log n⌋₊, (n + i) }.Infinite := by
-  sorry
